6d0eefab9e300985a7c8fc42150e1c577c081a85
ambscara <150319615+ambscara@users.noreply.github.com>
2024-02-04 13:19:02 -0800

    fixed code

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 function newImage(path, left, bottom) {
-    let image = document.createElement('img');
-    image.src = path;
-    image.style.position = 'fixed';
-    image.style.left = left + 'px'; // Make sure to add 'px' for left and bottom values
-    image.style.bottom = bottom + 'px';
-    document.body.append(image);
-  }
-  
-  newImage('assets/tree.png', 200, 300);
-  newImage('assets/pillar.png', 350, 100);
-  newImage('assets/crate.png', 150, 200);
-  newImage('assets/well.png', 500, 425);
-  
-  let sword = document.createElement('img');
-  sword.src = 'assets/sword.png';
-  sword.style.position = 'fixed'; // Fix: Use style.position instead of position
-  sword.style.left = '500px';
-  sword.style.bottom = '405px';
-  document.body.append(sword);
-  
-  sword.addEventListener('dblclick', function () {
-    sword.remove();
-  });
-  
-  function newItem(path, left, bottom) {
-    let item = document.createElement('img');
-    item.src = path;
-    item.style.position = 'fixed';
-    item.style.left = left + 'px';
-    item.style.bottom = bottom + 'px';
-    document.body.append(item);
-  }
-  
-  newItem('assets/sword.png', 500, 405);
-  newItem('assets/shield.png', 165, 185);
-  newItem('assets/staff.png', 600, 100);
+  let object = document.createElement('img');
+  object.src = path;
+  object.style.position = 'fixed';
+  object.style.left = left + 'px';
+  object.style.bottom = bottom + 'px';
+  document.body.append(object);
+  return object;
+}
 
- 
+function newItem(path, left, bottom) {
+  let item = newImage(path, left, bottom);
+
+  item.addEventListener('dblclick', function () {
+    item.remove();
+  });
+
+  return item;
+}
+
+
+let greenCharacter = newImage('assets/green-character.gif', 100, 100);
+let pineTree = newImage('assets/pine-tree.png', 450, 200);
+
+
+let tree = newImage('assets/tree.png', 200, 300);
+let pillar = newImage('assets/pillar.png', 350, 100);
+let crate = newImage('assets/crate.png', 150, 200);
+let well = newImage('assets/well.png', 500, 425);
+
+
+let sword = newItem('assets/sword.png', 500, 405);
+let shield = newItem('assets/shield.png', 165, 185);
+let staff = newItem('assets/staff.png', 600, 100);
